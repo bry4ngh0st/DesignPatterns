7bb12e7e66c10fb20dc00a73a7293b46cc44281e
ecastaneda <edson.castaneda@cignium.com>
2016-07-07 19:56:39 -0500

    Creacion del objeto producto y el manejador

---
+++ Unamba.DessignPatterns.Test01/Unamba.DessignPatterns.Test01/Manager/IProductoHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unamba.DessignPatterns.Test01.Manager
+{
+    public interface IProductoHandler
+    {
+        Producto Get(string id);
+        List<Producto> GetAll();
+    }
+}
