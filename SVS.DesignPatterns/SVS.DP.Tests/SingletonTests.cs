fbdcccf2e705ef05b5380131cba21a949eb4fe5e
ecastaneda <edson.castaneda@cignium.com>
2017-05-02 12:12:36 -0500

    Upload singleton pattern

---
+++ SVS.DesignPatterns/SVS.DP.Tests/SingletonTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVS.DP.Tests
+{
+    [TestFixture]
+    public class SingletonTests
+    {
+        [Test]
+        public void Main()
+        {
+            var singleton = new Singleton();
+
+            Assert.IsNotNull(singleton.value);
+        }
+
+        internal class Singleton
+        {
+            public string value { get; set; }
+
+            public Singleton()
+            {
+                if (value == null)
+                    value = string.Empty;
+            }
+        }
+    }
+}
