2b92d54d2c8007292ea0ab90fab062729fc7a2e8
ecastaneda <edson.castaneda@cignium.com>
2017-05-04 13:03:09 -0500

    Refactoring UnitOfWork sample

--- SVS.DesignPatterns/SVS.DP.Tests/UnitOfWorkTests.cs
+++ SVS.DesignPatterns/SVS.DP.Tests/UnitOfWorkTests.cs
@@ -12,12 +12,23 @@ namespace SVS.DP.Tests
     public class UnitOfWorkTests
     {
         private List<User> users;
+        private UnitOfWork.UnitOfWork unitOfWork;
 
         [SetUp]
         public void SetUp()
         {
             users = new List<User>();
+
             users.Add(new User { Id = 1, Name = "Percy", Address = string.Empty });
+            users.Add(new User { Id = 2, Name = "Kevin", Address = string.Empty });
+            users.Add(new User { Id = 3, Name = "Max", Address = string.Empty });            
+            users.Add(new User { Id = 4, Name = "Arthur", Address = string.Empty });
+            users.Add(new User { Id = 5, Name = "Carlos", Address = string.Empty });
+            users.Add(new User { Id = 6, Name = "Yordy", Address = string.Empty });
+
+            unitOfWork = new UnitOfWork.UnitOfWork();
+
+            unitOfWork.ExistingUsers = users;
         }
 
         [TestCase(1, "Percy")]
@@ -31,7 +42,7 @@ namespace SVS.DP.Tests
 
         [TestCase(0, "Percy")]
         [TestCase(1, "")]
-        public void WhenUserHasIncorrectDataValidationReturnsFalse(int id, string name)
+        public void WhenUserHasIncorrectDataValidationThrowsAnException(int id, string name)
         {
             var invalidUser = new User { Id = id, Name = name };
 
@@ -39,9 +50,17 @@ namespace SVS.DP.Tests
         }
 
         [Test]
+        public void WhenUserAlreadyExistsAddFeatureThrowsAnException()
+        {
+            var existingUser = unitOfWork.ExistingUsers.Find(u => u.Id.Equals(1));
+
+            Assert.Throws<Exception>(() => unitOfWork.AddUser(existingUser));
+        }
+
+        [Test]
         public void Main()
         {
-            var user = new User();
+            
         }
     }
 }
